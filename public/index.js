e2e17d2cdf8e0e7c6ea98fec7ed5d3f7d017bbe7
afeiship <1290657123@qq.com>
2019-12-17 21:27:55 +0800

    add: translate-y effect

--- public/index.js
+++ public/index.js
@@ -12,7 +12,7 @@ class App extends React.Component {
           <img src="http://himg.bdimg.com/sys/portrait/item/be10475f686d6c73db00.jpg" width="100%" />
         </ReactCard>
 
-        <ReactCard>
+        <ReactCard hoverable>
           <p>少年时，想碰到一个聂小倩，拼了性命爱一场，天亮前带着她的魂魄远走他乡。</p>
           <p>青年时，想碰到一个白素贞，家大业大，要啥有啥，吃完软饭一抹嘴，还有人负责把她关进雷峰塔。</p>
           <p>中年时，想要一个田螺姑娘，温婉可人，红袖添香，半夜写累了，让她变回原形，加干辣椒、花椒、姜、蒜片爆炒，淋入香油，起锅装盘。</p>
